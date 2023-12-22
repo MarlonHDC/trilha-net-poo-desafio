ebd42a075bff2e0816e5a8d38b9f7d6309672fe6
MarlonHDC <marlon_hdc@hotmail.com>
2023-12-22 11:02:18 -0300

    Primeiro Commit

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "3216-5498", modelo: "Modelo a32", imei: "111111", memoria: 256);
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+
+
+Console.WriteLine("\n");
+
